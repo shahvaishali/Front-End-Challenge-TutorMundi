96b38f8eaa65c02a04e1eb2b0815764bdf0339af
Vaishali Shah <shahvaishali2103@gmail.com>
2020-02-11 09:03:52 -0800

    Initial submitpraise component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Praise from "./Components/Praise/Praise";
-
+import SubmitPraise from "./Components/SubmitPraise/SubmitPraise";
 function App() {
   return (
     <div>
       <Header />
       <Praise />
+      <SubmitPraise />
     </div>
   );
 }
---
+++ src/Components/SubmitPraise/SubmitPraise.js
@@ -0,0 +1,11 @@
+import React from "react";
+export default function SubmitPraise() {
+  return (
+    <div>
+      <div>
+        <button>PULAR</button>
+        <button>CONFIRMAR</button>
+      </div>
+    </div>
+  );
+}
