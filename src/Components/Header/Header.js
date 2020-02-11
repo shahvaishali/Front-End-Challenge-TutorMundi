f54382d49834a14b67b213e9729d2df46aa034ed
Vaishali Shah <shahvaishali2103@gmail.com>
2020-02-11 00:20:06 -0800

    Added style to header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,23 +1,27 @@
 import React, { Component } from "react";
 import book from "./img/book.png";
 import { ReactComponent as Star } from "./img/star.svg";
+import "../Header/Header.css";
+import "../../App.css";
 
 export default class Header extends Component {
   render() {
     const star = [];
     for (let i = 0; i < 5; i++) {
-      star.push(<Star />);
+      star.push(<Star className="star ml-12" />);
     }
 
     return (
       <div>
-        <div>
-          <img src={book} alt="book" />
-        </div>
-        <div>
-          <p>Como foi a ajuda do tutor?</p>
-        </div>
-        <div>{star}</div>
+        <section className="center">
+          <div>
+            <img src={book} alt="book" className="book" />
+          </div>
+          <div>
+            <p className="text-title mb-24">Como foi a ajuda do tutor?</p>
+          </div>
+          <div>{star}</div>
+        </section>
       </div>
     );
   }
