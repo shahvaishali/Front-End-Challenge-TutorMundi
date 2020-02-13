fcc81a94de3ed303237d9fe82e730c6550e2b73f
Vaishali Shah <shahvaishali2103@gmail.com>
2020-02-12 18:22:37 -0800

    Added style to thankyou component

--- src/Components/ThankYou/ThankYou.js
+++ src/Components/ThankYou/ThankYou.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "../../App.css";
-
+import thankYou from "./img/thankyou.png";
 export default function ThankYou() {
   return (
-    <div>
+    <div className="center">
       <div>
-        <p>Obrigado pelo seu elogio!</p>
+        <img src={thankYou} alt="thankyou" className="mt-69" />
+      </div>
+      <div>
+        <p className="text-title mt-0">Obrigado pelo seu elogio!</p>
       </div>
     </div>
   );
